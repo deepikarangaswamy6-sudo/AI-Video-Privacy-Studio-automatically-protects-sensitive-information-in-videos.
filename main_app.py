19bca9153fb24d78d8a68e5eff299044a18f38ff
deepikarangaswamy6-sudo <deepikarangaswamy6@gmail.com>
2026-07-17 15:26:59 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import cv2
 import os
 import numpy as np
-from moviepy.editor import ImageSequenceClip
+from moviepy import ImageSequenceClip
 
 # ----------------------------
 # Create required folders
